09cacc55b55eb73ba9b1678bbccc5e926a0526bb
baggazzani <baggazzani@systel.com.ar>
2022-07-21 15:50:53 -0300

    jue 21 jul 1550

--- server/controllers/menu.price.controllers.js
+++ server/controllers/menu.price.controllers.js
@@ -1,15 +1,13 @@
 import {pool} from '../DB/connect';
 import CryptoJS from 'crypto-js';
 
+
+let product = '';
 export const getSearchPriceList = async(req, res)=>{
-    console.log('Entrando en getSearchPriceList...');
+    product = '';
     try{
         req = await pool.query("SELECT pricelist_version_id AS id, name FROM public.pricelist_version WHERE pricelist_id != '#'");       
             if (req.rows) {
-                console.log('');
-                console.log('Mostrando registros encontrados...');
-                console.log('');
-                console.log('Primer resultado: name = '+req.rows[0].name+' id = '+req.rows[0].id);
                 const resultado = {priceList: req.rows};                    
                 res
                 .set("Content-Security-Policy", "script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
@@ -18,16 +16,15 @@ export const getSearchPriceList = async(req, res)=>{
                 console.log('');
                 res.status(404).send({message: 'No hay registros!'});
                 console.log('');
-            }
-            //pool.end();      
+            }      
     } catch (e){
         console.log('');
         res.status(500).send('<h1>Pifiada del servidor!!</h1>');        
         console.log('');
         console.log('Falló ejecución de query');
-        //pool.end();
     }   
 };
+
 
 export const getTablePriceList = async(req, res)=>{
 
@@ -35,49 +32,36 @@ export const getTablePriceList = async(req, res)=>{
     const data = req.body;
     let pricelist_name = '';
     let pricelist_id = '';
-    let product = '';
+    //let product = '';
 
     pricelist_name = data.priceList_name;
     pricelist_id = data.priceList_id;
     if (data.product){
         product = data.product;
     }
-    
-    console.log('priceList name: '+pricelist_name);
-    console.log('priceList id: '+pricelist_id);
-    console.log('product: '+product);
-
     try{
         if (product != ''){
-            req = await pool.query("SELECT pp.product_id as id, p.name as name, round(pp.pricelist, 2) as price FROM public.productprice pp JOIN public.product p ON pp.product_id = p.product_id WHERE pricelist_version_id = '"+pricelist_id+"' AND p.name ILIKE '"+product+"%' ORDER BY pp.product_id");       
-            if (req.rows) {
-                console.log('');
-                console.log('Mostrando registros encontrados...');
-                console.log('');
-                console.log('Primer resultado: ID= '+req.rows[0].product_id+' PLU= '+req.rows[0].name+' Precio= '+req.rows[0].pricelist);
-                const resultado = {priceList: req.rows};                    
+            req = await pool.query("SELECT pp.product_id as id, p.name as name, round(pp.pricelist, 2) as price,  pp.pricelist_version_id as listprice FROM public.productprice pp JOIN public.product p ON pp.product_id = p.product_id WHERE pp.pricelist_version_id = '"+pricelist_id+"' AND p.name ILIKE '"+product+"%' ORDER BY pp.product_id");       
+            if (req.rows.length > 0) {
+                const resultado = {priceList: req.rows}; 
                 res
                 .set("Content-Security-Policy", "script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
                 .status(200).render('Price/table.ejs', {data: resultado});
             } else {
                 console.log('');
-                res.status(404).send({message: 'No hay registros!'});
+                res.status(404).send('<h2 style="position: relative; top: 45%; padding: 5%; text-align: center">La búsqueda no arrojó resultados.<br/>No sea boludo, ¡escriba bién!</h2>');
                 console.log('');
             }
         } else if (product == ''){
-            req = await pool.query("SELECT pp.product_id as id, p.name as name, round(pp.pricelist, 2) as price FROM public.productprice pp JOIN public.product p ON pp.product_id = p.product_id WHERE pricelist_version_id = '"+pricelist_id+"' ORDER BY pp.product_id");       
-            if (req.rows) {
-                console.log('');
-                console.log('Mostrando registros encontrados...');
-                console.log('');
-                console.log('Ejemplo de un resultado: ID= '+req.rows[0].product_id+' PLU= '+req.rows[0].name+' Precio= '+req.rows[0].pricelist);
-                const resultado = {priceList: req.rows};                    
+            req = await pool.query("SELECT pp.product_id as id, p.name as name, round(pp.pricelist, 2) as price, pp.pricelist_version_id as listprice FROM public.productprice pp JOIN public.product p ON pp.product_id = p.product_id WHERE pp.pricelist_version_id = '"+pricelist_id+"' ORDER BY pp.product_id");       
+            if (req.rows.length > 0) {
+                const resultado = {priceList: req.rows};               
                 res
                 .set("Content-Security-Policy", "script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
                 .status(200).render('Price/table.ejs', {data: resultado});
             } else {
                 console.log('');
-                res.status(404).send({message: 'No hay registros!'});
+                res.status(404).send('<h2 style="position: relative; top: 45%; padding: 5%; text-align: center">La búsqueda no arrojó resultados.<br/>No sea boludo, ¡escriba bién!</h2>');
                 console.log('');
             }
         }                  
@@ -90,21 +74,27 @@ export const getTablePriceList = async(req, res)=>{
 };
 
 export const getTableChanged = async(req, res)=>{
-    console.log('Entrando en getTableChanged...');
-
-    // Scá va el update a la tabla.
+    console.log('Entrando en getTableChanged...');    
 
     const data = req.body;
-    let pricelist_id = data.id;
-    console.log('ID obtenido: '+pricelist_id);
+    let id = data.id;
+    let price = data.newPrice;
+    let listprice = data.listprice;
+
     try{
-        req = await pool.query("SELECT pp.product_id as id, p.name as name, round(pp.pricelist, 2) as price FROM public.productprice pp JOIN public.product p ON pp.product_id = p.product_id WHERE pricelist_version_id = '"+pricelist_id+"' ORDER BY pp.product_id");       
+        req = await pool.query("UPDATE productprice SET pricelist = "+price+", updated = NOW() WHERE product_id = '"+id+"' AND pricelist_version_id = '"+listprice+"'");                 
+    } catch (e){
+        console.log('');
+        res.status(500).send('<h1>Pifiada del servidor!!</h1>');        
+        console.log('');
+        console.log('Falló ejecución de query');
+    }
+    
+    try{
+        if (product != ''){
+            req = await pool.query("SELECT pp.product_id as id, p.name as name, round(pp.pricelist, 2) as price,  pp.pricelist_version_id as listprice FROM public.productprice pp JOIN public.product p ON pp.product_id = p.product_id WHERE pp.pricelist_version_id = '"+listprice+"' AND p.name ILIKE '"+product+"%' ORDER BY pp.product_id");       
             if (req.rows) {
-                console.log('');
-                console.log('Mostrando registros encontrados...');
-                console.log('');
-                console.log('Ejemplo de un resultado: ID= '+req.rows[0].id+' PLU= '+req.rows[0].name+' Precio= '+req.rows[0].price);
-                const resultado = {priceList: req.rows};                    
+                const resultado = {priceList: req.rows}; 
                 res
                 .set("Content-Security-Policy", "script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
                 .status(200).render('Price/table.ejs', {data: resultado});
@@ -113,12 +103,23 @@ export const getTableChanged = async(req, res)=>{
                 res.status(404).send({message: 'No hay registros!'});
                 console.log('');
             }
-            //pool.end();      
+        } else if (product == ''){
+            req = await pool.query("SELECT pp.product_id as id, p.name as name, round(pp.pricelist, 2) as price, pp.pricelist_version_id as listprice FROM public.productprice pp JOIN public.product p ON pp.product_id = p.product_id WHERE pp.pricelist_version_id = '"+pricelist_id+"' ORDER BY pp.product_id");       
+            if (req.rows) {
+                const resultado = {priceList: req.rows};               
+                res
+                .set("Content-Security-Policy", "script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
+                .status(200).render('Price/table.ejs', {data: resultado});
+            } else {
+                console.log('');
+                res.status(404).send({message: 'No hay registros!'});
+                console.log('');
+            }
+        }      
     } catch (e){
         console.log('');
         res.status(500).send('<h1>Pifiada del servidor!!</h1>');        
         console.log('');
         console.log('Falló ejecución de query');
-        //pool.end();
     }   
 };
