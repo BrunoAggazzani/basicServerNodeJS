7b47f34e8502167cd1c0ae3f5f419c4cbac5e375
baggazzani <baggazzani@systel.com.ar>
2022-08-17 15:18:23 -0300

    mie 17 ago 1518

--- server/controllers/menu.abm.plu.controllers.js
+++ server/controllers/menu.abm.plu.controllers.js
@@ -1,8 +1,40 @@
-//import {pool} from '../DB/connect';
+import {pool} from '../DB/connect';
 //import CryptoJS from 'crypto-js';
 
-export const getAbmPlu = (req, res)=>{
-    res
-    .set("Content-Security-Policy", "script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
-    .status(200).render('ABM/Plu/plu1_0.ejs');
+export const getAbmPlu = async (req, res)=>{
+
+    const data = req.body;
+
+    let PLUlist = [];
+
+    try{
+        req = await pool.query("SELECT p.product_id AS id, p.name AS name, p.erp_code AS erp, p.isactive AS activo, p.description AS descripción, p.department_id, d.name AS departamento, p.group_id, g.name AS grupo, p.attribute AS tipo, p.tare AS tara FROM public.product p JOIN public.department d ON p.department_id = d.department_id JOIN public. main_group g ON p.group_id = g.group_id WHERE p.product_id != '0'");       
+            if (req.rows.length > 0) {
+                req.rows.map((e)=>{
+                    PLUlist.push(e);
+                });
+                console.log('PLUlist: '+JSON.stringify(PLUlist[0]));
+
+                const result = JSON.stringify(PLUlist);
+                    
+                res
+                .set("Content-Security-Policy", "script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
+                .status(200).render('ABM/Plu/plu1_0.ejs', {data: result});
+                
+            } else {
+                console.log('');
+                res.status(404).send({message: 'No hay registros!'});
+                console.log('');
+            }      
+    } catch (e){
+        console.log('');
+        res.status(500).send('<h1>Pifiada del servidor!!</h1>');        
+        console.log(e);
+        console.log('Falló ejecución de query');
+    }
+
+    if (data.PLU) {
+        console.log('lalala');
+    }
+
 };
